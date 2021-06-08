69f1c95ac9d897aaed6d0cfd6121ba3b34bb4ea3
iceandrise <kartinezmiss@gmail.com>
2021-06-08 19:10:12 +0300

    Added 3,4 labs

---
+++ lab3/web_app/blog/views.py
@@ -0,0 +1,78 @@
+import logging
+from django.contrib import messages
+from django.http import Http404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .models import Post, Category
+
+logger = logging.getLogger('django')
+
+
+class BlogListView(ListView):
+    paginate_by = 2
+    model = Post
+    template_name = 'home.html'
+
+    queryset = Post.objects.order_by('-publication_date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_categories'] = Category.objects.all()
+        return context
+    #queryset = Book.objects.filter(publisher__name='Acme Publishing')
+
+
+class CategoryPostList(ListView):
+    paginate_by = 2
+    template_name = 'posts_by_category.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(categories__title=self.kwargs['category'])
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'body', 'categories']
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super(BlogCreateView, self).form_valid(form)
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body', 'categories']
+
+    def get_object(self, queryset=None):
+        obj = super(BlogUpdateView, self).get_object(queryset)
+        if obj.author != self.request.user:
+            messages.error(self.request, "You can't edit this post")
+            logger.error("Attempt to get an access to update function")
+            raise Http404("You don't own this object")
+        return obj
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        obj = super(BlogDeleteView, self).get_object(queryset)
+        if obj.author != self.request.user:
+            messages.error(self.request, "You can't edit this post")
+            logger.error("Attempt to get an access to delete function")
+            raise Http404("You don't own this object")
+        return obj
+
+
